32eb341a177b9dcc55370d3abe745efe19a76224
Németh Richárd <richard.nemeth@maxolutions.hu>
2020-09-28 21:49:59 +0200

    - GoogleUAuth2Util created for auth url generation. - AuthController modified to return the url this way.

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,19 +1,15 @@
 import {Request, Response} from 'express';
-import {google} from 'googleapis';
 
 import {BaseController} from './base.controller';
 
 import {RouteConstants} from '../utils/constants/route.constants';
-import { Configuration } from '../utils/configuration/configuration';
+
+import {GoogleOAuth2Util} from '../models/utils/GoogleOAuth2Util';
 
 export class AuthController extends BaseController {
 
-  private googleOauth2Client;
-
   public constructor() {
     super();
-
-    this.createGoogleAuthClient();
 
     this.initAuthPath();
     this.initAuthRedirectPath();
@@ -21,28 +17,13 @@ export class AuthController extends BaseController {
 
   private initAuthPath(): void {
     this.router.get(RouteConstants.Auth.AUTH, (request: Request, response: Response) => {
-      response.json(this.createAuthUrl());
+      response.json(GoogleOAuth2Util.getGoogleAuthUrl());
     });
   }
 
   private initAuthRedirectPath(): void {
     this.router.get(RouteConstants.Auth.AUTH_SUCCESS, (request: Request, response: Response) => {
       console.log('success');
-    });
-  }
-
-  private createGoogleAuthClient() {
-    this.googleOauth2Client = new google.auth.OAuth2(
-      Configuration.GOOGLE_OAUTH_CONFIG.clientId,
-      Configuration.GOOGLE_OAUTH_CONFIG.clientSecret,
-      Configuration.GOOGLE_OAUTH_CONFIG.redirectUrl
-    );
-  }
-
-  private createAuthUrl(): string {
-    return this.googleOauth2Client.generateAuthUrl({
-      access_type: 'offline',
-      scope: Configuration.GOOGLE_CLASSROOM_SCOPES
     });
   }
 }
---
+++ src/models/utils/GoogleOAuth2Util.ts
@@ -0,0 +1,30 @@
+import {google} from 'googleapis';
+
+import {Configuration} from '../../utils/configuration/configuration';
+
+export class GoogleOAuth2Util {
+
+  private constructor() {
+  }
+
+  public static getGoogleAuthUrl(): string {
+    let googleAuthClient = GoogleOAuth2Util.createGoogleAuthClient();
+
+    return GoogleOAuth2Util.createAuthUrl(googleAuthClient);
+  }
+
+  private static createGoogleAuthClient() {
+    return new google.auth.OAuth2(
+      Configuration.GOOGLE_OAUTH_CONFIG.clientId,
+      Configuration.GOOGLE_OAUTH_CONFIG.clientSecret,
+      Configuration.GOOGLE_OAUTH_CONFIG.redirectUrl
+    );
+  }
+
+  private static createAuthUrl(googleAuthClient): string {
+    return googleAuthClient.generateAuthUrl({
+      access_type: 'offline',
+      scope: Configuration.GOOGLE_CLASSROOM_SCOPES
+    });
+  }
+}
