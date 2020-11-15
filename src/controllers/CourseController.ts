666b3c70ca43a08904ade4829eab5ca755449a47
Németh Richárd <richard.nemeth@maxolutions.hu>
2020-11-15 12:58:25 +0100

    - Basic validation implemented for request.

--- src/controllers/CourseController.ts
+++ src/controllers/CourseController.ts
@@ -63,9 +63,44 @@ export class CourseController extends BaseController {
 
   private initUploadCourseStudents(): void {
     this.router.post(RouteConstants.Courses.POST_COURSE_STUDENTS, async (request: Request, response: Response) => {
-      StudentsUtil.storeStudentsInDb(request.files[0].buffer);
+      let courseId: String;
+      let fileContent: Buffer;
+
+      try {
+        courseId = this.getCourseIdFromRequest(request);
+        fileContent = this.getFileContentFromRequest(request);
+      } catch(error: any) {
+        ApplicationLogger.errorLog({
+          tag: CourseController.TAG,
+          message: error
+        });
+
+        response.sendStatus(500);
+
+        return;
+      }
 
       response.sendStatus(200);
     });
+  }
+
+  private getCourseIdFromRequest(request: Request): string {
+    const courseId: string = request.body.courseId;
+
+    if (!courseId) {
+      throw new Error('Invalid courseId for uploadCourseStudents!');
+    }
+  
+    return courseId;
+  }
+
+  private getFileContentFromRequest(request: Request): Buffer {
+    const fileContent: Buffer = request.files[0];
+
+    if (!fileContent) {
+      throw new Error('Invalid fileContent for uploadCourseStudents!');
+    }
+
+    return fileContent;
   }
 }
