a7e285382deb42159fbac220b7978b5b1cfc9217
Németh Richárd <richard.nemeth@maxolutions.hu>
2020-11-16 18:34:52 +0100

    - Removed an unnecessary data type.

--- src/models/utils/GoogleCourse.ts
+++
@@ -1,6 +0,0 @@
-import {GaxiosResponse} from "gaxios";
-import {classroom_v1} from "googleapis";
-
-export interface GoogleCourse {
-  course: GaxiosResponse<classroom_v1.Schema$ListCoursesResponse>
-}
